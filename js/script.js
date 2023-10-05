d697b5d04fa388c24527c3c5abe12b94466f6207
felipesinn <felipesinn01@gmail.com>
2023-10-04 23:36:35 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+
+  function hideElements() {
+    const marvelElement = document.getElementById('marvel');
+    const studiosElement = document.getElementById('studios');
+    const apresentaElement = document.getElementById('apresenta');
+
+    const tempoParaOcultar = 5000; 
+
+    setTimeout(function() {
+      marvelElement.style.display = 'none';
+      studiosElement.style.display = 'none';
+      apresentaElement.style.display = 'none';
+    }, tempoParaOcultar);
+  }
+
+
+  // Chama a função para iniciar a contagem regressiva
+  hideElements();
+
+
+
+
+function onAnimationsEnd() {
+   
+    document.getElementById('animate').removeEventListener('animationend', onAnimationsEnd);
+
+    setTimeout(function() {
+        document.body.style.backgroundImage = 'url(../img/pader01.jpg)';
+        document.body.style.backgroundSize = 'cover'; 
+
+        const texto = document.createElement('p');
+        texto.textContent = ''; 
+
+       
+        document.body.appendChild(texto);
+    }, 5000);
+}
+
+document.getElementById('animate').addEventListener('animationend', onAnimationsEnd);
